56f033701ec7fc7e5e85099865eed882487b1d80
Home <1525714537@qq.com>
2019-06-15 23:34:04 +0800

    UDAF

--- src/main/scala/com/scala/spark/local/v1/sql/udaf/AvgSalary.scala
+++ src/main/scala/com/scala/spark/local/v1/sql/udaf/AvgSalary.scala
@@ -1,43 +1,31 @@
 package com.scala.spark.local.v1.sql.udaf
 
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
-import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Encoder, Encoders}
+import org.apache.spark.sql.expressions.Aggregator
 
-/**
-  * 弱类型聚合
-  */
-object AvgSalary extends UserDefinedAggregateFunction{
 
-  //输入参数的数据类型
-  override def inputSchema: StructType = StructType(StructField("salary",LongType,true)::Nil)
-  //保存业务数据的数据结构
-  override def bufferSchema: StructType = StructType(StructField("sum",LongType,true)::StructField("count",IntegerType,true)::Nil)
-  //返回值的数据类型
-  override def dataType: DataType = DoubleType
-  //相同的输入有相同的输出
-  override def deterministic: Boolean = true
-  //初始化
-  override def initialize(buffer: MutableAggregationBuffer): Unit = {
-    // 存工资的总额
-    buffer(0) = 0L
-    // 存工资的个数
-    buffer(1) = 0
+
+case class Employee(name:String,salary: Long)
+case class Average(var sum:Long,var count:Int)
+object AvgSalary extends Aggregator[Employee,Average,Double]{
+  //定义一个数据结构，保存工资总数和工资总个数，初始都为0
+  override def zero: Average = Average(0L,0)
+
+  override def reduce(b: Average, a: Employee): Average = {
+     b.sum+=a.salary
+     b.count+=1
+     b
   }
-  //同分区内row对聚合函数的操作
-  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
-    if(!input.isNullAt(0)){
-      buffer(0)=buffer.getLong(0)+input.getLong(0)
-      buffer(1)=buffer.getInt(1)+1
-    }
+
+  override def merge(b1: Average, b2: Average): Average = {
+    b1.sum+=b2.sum
+    b1.count+=b2.count
+    b1
   }
-  //不同分区的聚合结果的合并
-  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
-    buffer1(0)=buffer1.getLong(0)+buffer2.getLong(0)
-    buffer1(1)=buffer1.getInt(1)+buffer2.getInt(1)
-  }
-  //计算最终结果
-  override def evaluate(buffer: Row): Any = {
-    buffer.getLong(0).toDouble/buffer.getInt(1)
-  }
+
+  override def finish(reduction: Average): Double = reduction.sum.toDouble/reduction.count
+  //Encoders.product是进行scala元组和case类转换的编码器
+  override def bufferEncoder: Encoder[Average] = Encoders.product
+  //最终输出值的编码器
+  override def outputEncoder: Encoder[Double] = Encoders.scalaDouble
 }
--- src/main/scala/com/scala/spark/local/v1/sql/udaf/UDAFDemo.scala
+++ src/main/scala/com/scala/spark/local/v1/sql/udaf/UDAFDemo.scala
@@ -3,17 +3,30 @@ package com.scala.spark.local.v1.sql.udaf
 import com.scala.spark.local.v1.sql.RDDToDataFrameDemo
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-case class Employee(name:String,salary: Long)
+
 object UDAFDemo {
   def main(args: Array[String]): Unit = {
     val sparkConf=new SparkConf().setMaster("local[*]").setAppName(RDDToDataFrameDemo.getClass.getSimpleName)
     val sparkSession=SparkSession.builder().config(sparkConf).getOrCreate()
+    //weakUDAF(sparkSession)
+    udaf(sparkSession)
+  }
+
+  def udaf(sparkSession:SparkSession): Unit ={
     import sparkSession.implicits._
-    sparkSession.udf.register("avgSalary",AvgSalary)
+    val rdd=sparkSession.sparkContext.makeRDD(List(("neo",1000),("tom",1500),("jack",2100)))
+    val userDS=rdd.map(tuple=>Employee(tuple._1,tuple._2)).toDS()
+    val avgSalary=AvgSalary.toColumn.name("avgSalary")
+    val result=userDS.select(avgSalary)
+    result.show()
+  }
+  def weakUDAF(sparkSession:SparkSession): Unit ={
+    import sparkSession.implicits._
+    sparkSession.udf.register("weekAvgSalary",WeakAvgSalary)
     val rdd=sparkSession.sparkContext.makeRDD(List(("neo",1000),("tom",1500),("jack",2100)))
     val userDF=rdd.map(tuple=>Employee(tuple._1,tuple._2)).toDF()
     userDF.createOrReplaceTempView("employees")
-    val result=sparkSession.sql("select avgSalary(salary) from employees")
+    val result=sparkSession.sql("select weekAvgSalary(salary) from employees")
     result.show()
   }
 }
